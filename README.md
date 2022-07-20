ed27a34a7bad096fc64ac9f2283f9f1ffe70ef3d
Gustavo <angstoficial@gmail.com>
2022-07-20 11:38:13 -0300

    🔌 First Commit

---
+++ README.md
@@ -0,0 +1,63 @@
+## FiveM Status
+API for getting FiveM server info & status now using node-fetch module.
+
+## Installation
+```
+npm install fivem-status
+npm i fivem-status
+```
+
+## Usage
+**Examples**
+
+See the server status (json)
+```js
+const FiveMStatus = require('fivem-status'); //Import the module
+const server = new FiveMStatus.Server('IPAddress:PORT'); //Set the server's IP Address and Port
+
+server.getServerStatus().then(data => console.log(data)); //Get & log the Server Status!
+
+//if online returns json {online: true}
+// if offline returns json {online: false}
+```
+
+Get Player Count (in numbers)
+```js
+const FiveMStatus = require('fivem-status'); //Import the module
+const server = new FiveMStatus.Server('IPAddress:PORT'); //Set the server's IP Address and Port
+ 
+server.getPlayers().then(data => console.log(data)) // Get & log the number of players online in the server!
+```
+
+Get all Resources
+```js
+const FiveMStatus = require('fivem-status'); //Import the module
+const server = new FiveMStatus.Server('IPAddress:PORT'); //Set the server's IP Address and Port
+ 
+server.getResources().then(data => console.log(data)) // Get & Log the resources of server!
+```
+
+
+## **ALL FUNCTION REQUESTS**
+```
+- getPlayers - Number of players online - (number)
+- getPlayersAll - List all players in an array - (string)
+- getMaxPlayers - Max players that are able to join the server - (number)
+- getResources - Get resource names of all server resources - (string/array)
+- getTags - Get all server tags - (string)
+- getServer - Get the whole server object - (string)
+- getOnesync - See if the server has OneSync enabled - (boolean)
+- getLocale - The language of the server - (string)
+- getGamename - Get the name of the server - (string)
+- getEnhancedHostSupport - ... - (boolean)
+- getlicenseKeyToken - The license key for the server - (string)
+- getScriptHookAllowed - See if the server supports external mod menus from the client - (boolean)
+- getBannerConnecting - Get the banner_connecting of the server - (string)
+- getBannerDetail - Get the banner_detail of the server - (string)
+- getServerDesc - Get server description (string) 
+```
+
+## Credits:
+This is a module that already existed but made using the axios module, I rewrote it using the node-fetch module and added some functions.
+https://github.com/JackCrispy/fivem - Official Module
+https://www.npmjs.com/package/fivem - Official package of npm
---
+++ index.js
@@ -0,0 +1,289 @@
+const fetch = require('node-fetch');
+
+class Status {
+	constructor(ip, options) {
+		if (!ip) throw Error('Please provide an IP.');
+
+		this.ip = ip;
+		this.options = Object.assign(options);
+	}
+
+	getPlayers() {
+		return new Promise((send, err) => {
+			fetch(`http://${this.ip}/players.json`, {
+				"method": "GET",
+				"timeout": "1000"
+			})
+				.then(res => res.json())
+				.then(body => {
+					let players = body;
+					send(players.length);
+				})
+				.catch(function (error) {
+					err(error);
+				});
+		});
+	}
+
+	getPlayersAll() {
+		return new Promise((send, err) => {
+			fetch(`http://${this.ip}/players.json`, {
+				"method": "GET",
+				"timeout": "1000"
+			})
+				.then(res => res.json())
+				.then(body => {
+					let players = body;
+					send(players);
+				})
+				.catch(function (error) {
+					err(error);
+				});
+		});
+	}
+
+	getServerStatus() {
+		return new Promise((send, err) => {
+			fetch(`http://${this.ip}/info.json`, {
+				"method": "GET",
+				"timeout": "1000"
+			})
+				.then(res => res.json())
+				.then(body => {
+					let server_status = {
+						online: true,
+					}
+					send(server_status);
+				})
+				.catch(function (error) {
+					let server_status = {
+						online: false
+					}
+					if (error.response == undefined) send(server_status)
+				});
+		});
+	}
+
+	getResources() {
+		return new Promise((send, err) => {
+			fetch(`http://${this.ip}/info.json`, {
+				"method": "GET",
+				"timeout": "1000"
+			})
+				.then(res => res.json())
+				.then(body => {
+					let resources = body.resources;
+					send(resources);
+				})
+				.catch(function (error) {
+					err(error);
+				});
+		});
+	}
+
+	getOnesync() {
+		return new Promise((send, err) => {
+			fetch(`http://${this.ip}/info.json`, {
+				"method": "GET",
+				"timeout": "1000"
+			})
+				.then(res => res.json())
+				.then(body => {
+					let onesync = body.vars.onesync_enabled;
+					send(onesync);
+				})
+				.catch(function (error) {
+					err(error);
+				});
+		});
+	}
+
+	getMaxPlayers() {
+		return new Promise((send, err) => {
+			fetch(`http://${this.ip}/info.json`, {
+				"method": "GET",
+				"timeout": "1000"
+			})
+				.then(res => res.json())
+				.then(body => {
+					let maxClients = body.vars.sv_maxClients;
+					send(maxClients);
+				})
+				.catch(function (error) {
+					err(error);
+				});
+		});
+	}
+
+	getLocale() {
+		return new Promise((send, err) => {
+			fetch(`http://${this.ip}/info.json`, {
+				"method": "GET",
+				"timeout": "1000"
+			})
+				.then(res => res.json())
+				.then(body => {
+					let locale = body.vars.locale;
+					send(locale);
+				})
+				.catch(function (error) {
+					err(error);
+				});
+		});
+	}
+
+	getGamename() {
+		return new Promise((send, err) => {
+			fetch(`http://${this.ip}/info.json`, {
+				"method": "GET",
+				"timeout": "1000"
+			})
+				.then(res => res.json())
+				.then(body => {
+					let gamename = body.vars.gamename;
+					send(gamename);
+				})
+				.catch(function (error) {
+					err(error);
+				});
+		});
+	}
+
+	getEnhancedHostSupport() {
+		return new Promise((send, err) => {
+			fetch(`http://${this.ip}/info.json`, {
+				"method": "GET",
+				"timeout": "1000"
+			})
+				.then(res => res.json())
+				.then(body => {
+					let enhancedHostSupport = body.vars.sv_enhancedHostSupport;
+					send(enhancedHostSupport);
+				})
+				.catch(function (error) {
+					err(error);
+				});
+		});
+	}
+
+	getlicenseKeyToken() {
+		return new Promise((send, err) => {
+			fetch(`http://${this.ip}/info.json`, {
+				"method": "GET",
+				"timeout": "1000"
+			})
+				.then(res => res.json())
+				.then(body => {
+					let licenseKeyToken = body.vars.sv_licenseKeyToken;
+					send(licenseKeyToken);
+				})
+				.catch(function (error) {
+					err(error);
+				});
+		});
+	}
+
+	getScriptHookAllowed() {
+		return new Promise((send, err) => {
+			fetch(`http://${this.ip}/info.json`, {
+				"method": "GET",
+				"timeout": "1000"
+			})
+				.then(res => res.json())
+				.then(body => {
+					let scriptHookAllowed = body.vars.sv_scriptHookAllowed;
+					send(scriptHookAllowed);
+				})
+				.catch(function (error) {
+					err(error);
+				});
+		});
+	}
+
+	getTags() {
+		return new Promise((send, err) => {
+			fetch(`http://${this.ip}/info.json`, {
+				"method": "GET",
+				"timeout": "1000"
+			})
+				.then(res => res.json())
+				.then(body => {
+					let tags = body.vars.tags;
+					send(tags);
+				})
+				.catch(function (error) {
+					err(error);
+				});
+		});
+	}
+
+	getServer() {
+		return new Promise((send, err) => {
+			fetch(`http://${this.ip}/info.json`, {
+				"method": "GET",
+				"timeout": "1000"
+			})
+				.then(res => res.json())
+				.then(body => {
+					let server = body;
+					send(server);
+				})
+				.catch(function (error) {
+					err(error);
+				});
+		});
+	}
+
+	getBannerConnecting() {
+		return new Promise((send, err) => {
+			fetch(`http://${this.ip}/info.json`, {
+				"method": "GET",
+				"timeout": "1000"
+			})
+				.then(res => res.json())
+				.then(body => {
+					let server = body.vars.banner_connecting;
+					send(server);
+				})
+				.catch(function (error) {
+					err(error);
+				});
+		});
+	}
+
+	getBannerDetail() {
+		return new Promise((send, err) => {
+			fetch(`http://${this.ip}/info.json`, {
+				"method": "GET",
+				"timeout": "1000"
+			})
+				.then(res => res.json())
+				.then(body => {
+					let server = body.vars.banner_detail;
+					send(server);
+				})
+				.catch(function (error) {
+					err(error);
+				});
+		});
+	}
+
+	getServerDesc() {
+		return new Promise((send, err) => {
+			fetch(`http://${this.ip}/info.json`, {
+				"method": "GET",
+				"timeout": "1000"
+			})
+				.then(res => res.json())
+				.then(body => {
+					let server = body.vars.sv_projectDesc;
+					send(server);
+				})
+				.catch(function (error) {
+					err(error);
+				});
+		});
+	}
+}
+
+module.exports.Status = Status;
